d4c8fbdca19eb8de560a65a4ff2549c35f9d8a28
Nathan Downey <nathandowney@Nathans-MacBook-Pro.local>
2020-04-13 08:18:42 -0500

    initial commit

---
+++ src/components/Sizes.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+const style = {
+    border: '1px solid white',
+    backgroundColor: 'white',
+    color: 'black',
+};
+
+const Size = () => {
+    const sizes = [
+        {name: "8 oz", price: "$10"},
+        {name: "16 oz", price: "$18"},
+        {name: "30 oz", price: "$30"},
+    ];
+
+    return( 
+        <div style={style}>
+            <h1>{sizes[0].name}</h1>
+            <p>{sizes[0].price}</p>
+        </div>
+    );
+    
+};
+
+export default Size;
